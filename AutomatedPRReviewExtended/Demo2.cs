84b9006985458c4f40e82d05d3bd3cac98c04b00
Utkarsh Prajapati <utkarsh.p@simformsolutions.com>
2025-05-27 13:27:12 +0530

    Test commit

--- AutomatedPRReviewExtended/Demo2.cs
+++ AutomatedPRReviewExtended/Demo2.cs
@@ -28,7 +28,7 @@ public class Demo2 // Already PascalCase
         }
         catch (Exception)
         {
-            // No logging or rethrow test
+            // No logging or rethrow test2
         }
         // select * from Users where password = '1234'; // Possible SQL injection, hardcoded password
         // This is commented-out code;
